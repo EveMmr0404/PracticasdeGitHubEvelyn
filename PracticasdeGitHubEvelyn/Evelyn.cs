f38b602a0d5117529fb5f6333c14ceebedd966fa
Evelyn Manuela Mosquera Ramirez <evemmosquera@soy.sena.edu.co>
2024-05-23 20:52:16 -0500

    AGREGE UNA CLASE CON MI NOMBRE

---
+++ PracticasdeGitHubEvelyn/Evelyn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticasdeGitHubEvelyn
+{
+    internal class Evelyn
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Y ESTA ES MI CLASE CON MI NOMBRE");
+        }
+    }
+}
